ad26ba8060846cee421c8515a34122cc204f2b3f
Sibeng32 <94640388+Sibeng32@users.noreply.github.com>
2022-05-18 20:12:05 +0200

    woensdag 18-5
    fits toegevoegd.

--- python golven en optica.py
+++ python golven en optica.py
@@ -89,7 +89,7 @@ plt.xlabel('n-de eigenfrequentie')
 plt.show()
 
 #%%
-#we gebruiken een csv omdat een xlsx niet moeilijker met numpy importeert.
+#we gebruiken een csv omdat een xlsx moeilijk met numpy importeert.
 filename = 'metingen eigen frequenties nikkel 0.3 - Sheet1.csv'
 nikkel_3 = np.genfromtxt(filename, delimiter=',')
 
@@ -112,85 +112,320 @@ plt.ylabel('frequentie in Hz')
 plt.xlabel('n-de eigenfrequentie')
 plt.show()
 
-#%%
+#%% gemiddeldes
+# zo vinden we gemiddeldes per eigenfreq per draad
 efk2 = (ef1k2+ef2k2+ef3k2)/3
 efk3 = (ef1k3+ef2k3+ef3k3)/3
 efk4 = (ef1k4+ef2k4+ef3k4)/3
 efn3 = (ef1n3+ef2n3+ef3n3)/3
 
+#%% ideale fit voor kopere draad van 0.2 mm (k2)
 
-#%%
-## (0) Importeer het ODR pakket (ODR = Orthogonal Distance Regression)
-import scipy.odr as odr
-import numpy as np
-import matplotlib.pyplot as plt
-	
-# Fictieve dataset x- en y-waarden, beide met onzekerheid
-x = np.array([1,2,3,4,5,6,7,8])
-sig_x = np.array([0.0001,0.0001,0.0001,0.0001,0.0001,0.0001,0.0001,0.0001])
+#nummer van eigenfreq van betreffende draad
+x = np.array([1,2,3,4,5,6,7,8]) 
+#gevonden eigenfrequenties
 y = efk2
-sig_y = np.array([1,1,1,1,1,1,1,1])
+
+#alle onzekerheden
+sig_x = np.array([0.0001,0.0001,0.0001,0.0001,0.0001,0.0001,0.0001,0.0001])
+#doordat we de meting 3 keer hebben uitgevoerd wordt de onzekerheid kleiner.
+sig_y = np.array([1,1,1,1,1,1,1,1])/2
 	
-# We maken aan deze data een fit met de functie y = A + B x
-# Het is goed om deze dataset te plotten, en op basis hiervan een schatting 
-# te maken voor startwaarden voor de parameters A en B. Voor deze 
-# specifieke dataset kunnen we als schatting voor A de waarde gemeten bij 
-# de asafsnede nemen (0.5 dus), en voor B, de helling, ongeveer 1.5
-# Met deze opzet kun je overigens eerst een plot maken met data (activeer 
-# dan ax.show), en op een later moment de beste rechte lijn erbij plotten
-# NB Dit is een niet opgemaakte plot
+# Dit is een niet opgemaakte sub plot voor de stippen
 f,ax = plt.subplots(1)
 ax.errorbar(x,y,xerr=sig_x,yerr=sig_y,fmt='k.')
 #ax.show()
 freq_start=0
 	
-## (1) Definieer een Python-functie die het model bevat, in dit geval een 
-## rechte lijn
-## B is een vector met parameters, in dit geval twee (A = B[0], B = B[1])
-## x is de array met x-waarden
+# de functie/relatie
 def f(x,freq):
     ef = x*freq
     return ef
 	
-## (2) Definieer het model-object om te gebruiken in odr
+#model-object dat we gebruiken in odr
 odr_model = odr.Model(f)
 	
-## (3) Definieer een RealData object
-## Een RealData-object vraagt om de onzekerheden in beide richtingen. 
-## !! De onzekerheid in de x-richting mag ook nul zijn (dan mag je 
-## sx=0 weglaten), maar dan moet bij onderdeel (4)/(4a) wel gekozen 
-## worden voor een kleinste-kwadratenfit !!
+#real data opbject
 odr_data  = odr.RealData(x,y,sx=sig_x,sy=sig_y)
 	
-## (4) Maak een ODR object met data, model en startwaarden
-## Je geeft startwaarden voor parameters mee bij keyword beta0
+# start waarde voor parameters
 odr_obj   = odr.ODR(odr_data,odr_model,beta0=[freq_start])
-## (4a) Stel in op kleinste kwadraten (optioneel)
-## Als de onzekerheden in de x-richting gelijk zijn aan nul, dan faalt de 
-## default-aanpak van ODR. Je moet dan als methode een kleinste-kwadraten- 
-## fit instellen. Dit gaat met het volgende commando (nu uit):
-#odr_obj.set_job(fit_type=2)
-	
-## (5) Voer de fit uit
-## Dit gebeurt expliciet door functie .run() aan te roepen
+
+
 odr_res   = odr_obj.run()
 	
-## (6) Haal resultaten uit het resultaten-object:
-# (6a) De beste schatters voor de parameters
+# de beste schatters voor de parameters
 par_best = odr_res.beta
-# (6b) De (EXTERNE!) onzekerheden voor deze parameters
+
+# de onzekerheden voor deze parameters
 par_sig_ext = odr_res.sd_beta
-# (6c) De (INTERNE!) covariantiematrix
+
+# covariantiematrix
 par_cov_ext = odr_res.cov_beta 
-# (6d) De chi-kwadraat en de gereduceerde chi-kwadraat van deze fit
+
+# De chi-kwadraat en de gereduceerde chi-kwadraat van deze fit
 chi2 = odr_res.sum_square
 chi2red = odr_res.res_var
-# (6e) Een compacte weergave van de belangrijkste resultaten als output
-odr_res.pprint()
 
-# Hier plotten we ter controle de fit met de dataset (niet opgemaakt)
+# Een compacte weergave van de resultaten als output
+# odr_res.pprint()
+
+# Hier plotten we ter controle de fit met de dataset met opmaak
+# En printen we de gereduceerde chi kwadraat uit.
+print('De gereduceerde chi-kwadraat van k2 is:', chi2red)
 xplot=np.linspace(1,8, 8)
 ax.plot(xplot, f([par_best[0]],xplot),'r-')
 
+#%% ideale fit voor kopere draad van 0.3 mm (k3)
+
+#nummer van eigenfreq van betreffende draad
+x = np.array([1,2,3,4,5,6,7,8]) 
+#gevonden eigenfrequenties
+y = efk3
+
+#alle onzekerheden
+sig_x = np.array([0.0001,0.0001,0.0001,0.0001,0.0001,0.0001,0.0001,0.0001])
+sig_y = np.array([1,1,1,1,1,1,1,1])/2
+	
+# Dit is een niet opgemaakte sub plot voor de stippen
+f,ax = plt.subplots(1)
+ax.errorbar(x,y,xerr=sig_x,yerr=sig_y,fmt='k.')
+#ax.show()
+freq_start=0
+	
+# de functie/relatie
+def f(x,freq):
+    ef = x*freq
+    return ef
+	
+#model-object dat we gebruiken in odr
+odr_model = odr.Model(f)
+	
+#real data opbject
+odr_data  = odr.RealData(x,y,sx=sig_x,sy=sig_y)
+	
+# start waarde voor parameters
+odr_obj   = odr.ODR(odr_data,odr_model,beta0=[freq_start])
 
 
+odr_res   = odr_obj.run()
+	
+# de beste schatters voor de parameters
+par_best = odr_res.beta
+
+# de onzekerheden voor deze parameters
+par_sig_ext = odr_res.sd_beta
+
+# covariantiematrix
+par_cov_ext = odr_res.cov_beta 
+
+# De chi-kwadraat en de gereduceerde chi-kwadraat van deze fit
+chi2 = odr_res.sum_square
+chi2red = odr_res.res_var
+
+# Een compacte weergave van de resultaten als output
+# odr_res.pprint()
+
+# Hier plotten we ter controle de fit met de dataset met opmaak
+# En printen we de gereduceerde chi kwadraat uit.
+print('De gereduceerde chi-kwadraat van k3 is:', chi2red)
+xplot=np.linspace(1,8, 8)
+ax.plot(xplot, f([par_best[0]],xplot),'r-')
+#%% ideale fit voor kopere draad van 0.4 mm (k4)
+
+#nummer van eigenfreq van betreffende draad
+x = np.array([1,2,3,4,5,6,7,8]) 
+#gevonden eigenfrequenties
+y = efk4
+
+#alle onzekerheden
+sig_x = np.array([0.0001,0.0001,0.0001,0.0001,0.0001,0.0001,0.0001,0.0001])
+sig_y = np.array([1,1,1,1,1,1,1,1])/2
+	
+# Dit is een niet opgemaakte sub plot voor de stippen
+f,ax = plt.subplots(1)
+ax.errorbar(x,y,xerr=sig_x,yerr=sig_y,fmt='k.')
+#ax.show()
+freq_start=0
+	
+# de functie/relatie
+def f(x,freq):
+    ef = x*freq
+    return ef
+	
+#model-object dat we gebruiken in odr
+odr_model = odr.Model(f)
+	
+#real data opbject
+odr_data  = odr.RealData(x,y,sx=sig_x,sy=sig_y)
+	
+# start waarde voor parameters
+odr_obj   = odr.ODR(odr_data,odr_model,beta0=[freq_start])
+
+
+odr_res   = odr_obj.run()
+	
+# de beste schatters voor de parameters
+par_best = odr_res.beta
+
+# de onzekerheden voor deze parameters
+par_sig_ext = odr_res.sd_beta
+
+# covariantiematrix
+par_cov_ext = odr_res.cov_beta 
+
+# De chi-kwadraat en de gereduceerde chi-kwadraat van deze fit
+chi2 = odr_res.sum_square
+chi2red = odr_res.res_var
+
+# Een compacte weergave van de resultaten als output
+# odr_res.pprint()
+
+# Hier plotten we ter controle de fit met de dataset met opmaak
+# En printen we de gereduceerde chi kwadraat uit.
+print('De gereduceerde chi-kwadraat van k4 is:', chi2red)
+xplot=np.linspace(1,8, 8)
+ax.plot(xplot, f([par_best[0]],xplot),'r-')
+#%% ideale fit voor kopere draad van 0.3 mm (n3)
+
+#nummer van eigenfreq van betreffende draad
+x = np.array([1,2,3,4,5,6,7,8]) 
+#gevonden eigenfrequenties
+y = efn3
+
+#alle onzekerheden
+sig_x = np.array([0.0001,0.0001,0.0001,0.0001,0.0001,0.0001,0.0001,0.0001])
+sig_y = np.array([1,1,1,1,1,1,1,1])/3
+	
+# Dit is een niet opgemaakte sub plot voor de stippen
+f,ax = plt.subplots(1)
+ax.errorbar(x,y,xerr=sig_x,yerr=sig_y,fmt='k.')
+#ax.show()
+freq_start=0
+	
+# de functie/relatie
+def f(x,freq):
+    ef = x*freq
+    return ef
+	
+#model-object dat we gebruiken in odr
+odr_model = odr.Model(f)
+	
+#real data opbject
+odr_data  = odr.RealData(x,y,sx=sig_x,sy=sig_y)
+	
+# start waarde voor parameters
+odr_obj   = odr.ODR(odr_data,odr_model,beta0=[freq_start])
+
+
+odr_res   = odr_obj.run()
+	
+# de beste schatters voor de parameters
+par_best = odr_res.beta
+
+# de onzekerheden voor deze parameters
+par_sig_ext = odr_res.sd_beta
+
+# covariantiematrix
+par_cov_ext = odr_res.cov_beta 
+
+# De chi-kwadraat en de gereduceerde chi-kwadraat van deze fit
+chi2 = odr_res.sum_square
+chi2red = odr_res.res_var
+
+# Een compacte weergave van de resultaten als output
+# odr_res.pprint()
+
+# Hier plotten we ter controle de fit met de dataset met opmaak
+# En printen we de gereduceerde chi kwadraat uit.
+print('De gereduceerde chi-kwadraat van n3 is:', chi2red)
+xplot=np.linspace(1,8, 8)
+ax.plot(xplot, f([par_best[0]],xplot),'r-')
+
+#%% lower en upper waardes voor in de young modulus
+m = 0.160 # massa aan draad in kg +- 0.0002 kg
+L = 1.453 # lengte draad in meter +- 0.005
+g = 9.81  # zwaartekrachtsversnelling in m/s^2 zonder onzekerheid
+Pk = 8960 # dichtheid koper in kg/m3
+Pn = 8902 # dichtheid nikkel in kg/m3
+Yk_mi = 121  # minimale youngs modulus van koper in Gpa
+Yk_ma = 133  # maximale youngs modulus van koper in Gpa
+Yn_mi = 190  # minimale youngs modulus van nikkel in Gpa
+Yn_ma = 220  # minimale youngs modulus van nikkel in Gpa
+sig_r = 0.000005 # onzekerheid van de straal van draad
+
+f_upperk2 = (1/(2*(L-0.005)))*((((m+0.0002)*g))/(np.pi*Pk*(0.0001-sig_r)**2))**(1/2)
+f_upperk3 = (1/(2*(L-0.005)))*((((m+0.0002)*g))/(np.pi*Pk*(0.00015-sig_r)**2))**(1/2)
+f_upperk4 = (1/(2*(L-0.005)))*((((m+0.0002)*g))/(np.pi*Pk*(0.0002-sig_r)**2))**(1/2)
+f_uppern3 = (1/(2*(L-0.005)))*((((m+0.0002)*g))/(np.pi*Pk*(0.00015-sig_r)**2))**(1/2)
+
+f_lowerk2 = (1/(2*(L+0.005)))*((((m-0.0002)*g))/(np.pi*Pk*(0.0001+sig_r)**2))**(1/2)
+f_lowerk3 = (1/(2*(L+0.005)))*((((m-0.0002)*g))/(np.pi*Pk*(0.00015+sig_r)**2))**(1/2)
+f_lowerk4 = (1/(2*(L+0.005)))*((((m-0.0002)*g))/(np.pi*Pk*(0.0002+sig_r))**2)**(1/2)
+f_uppern3 = (1/(2*(L+0.005)))*((((m-0.0002)*g))/(np.pi*Pk*(0.00015+sig_r)**2))**(1/2)
+
+B_upperk2 = (np.pi**3)*((0.0001-sig_r)**(4))*Yk_ma/(4*(m+0.0002)*9.81*(L+0.005)**2)
+B_upperk3 = (np.pi**3)*((0.00015-sig_r)**(4))*Yk_ma/(4*(m+0.0002)*9.81*(L+0.005)**2)
+B_upperk4 = (np.pi**3)*((0.0002-sig_r)**(4))*Yk_ma/(4*(m+0.0002)*9.81*(L+0.005)**2)
+B_uppern3 = (np.pi**3)*((0.00015-sig_r)**(4))*Yn_ma/(4*(m+0.0002)*9.81*(L+0.005)**2)
+
+B_lowerk2 = (np.pi**3)*((0.0001+sig_r)**(4))*Yk_ma/(4*(m-0.0002)*9.81*(L-0.005)**2)
+B_lowerk3 = (np.pi**3)*((0.00015+sig_r)**(4))*Yk_ma/(4*(m-0.0002)*9.81*(L-0.005)**2)
+B_lowerk4 = (np.pi**3)*((0.0002+sig_r)**(4))*Yk_ma/(4*(m-0.0002)*9.81*(L-0.005)**2)
+B_lowern3 = (np.pi**3)*((0.00015+sig_r)**(4))*Yn_ma/(4*(m-0.0002)*9.81*(L-0.005)**2)
+
+#%% fit voor kopere draad van 0.2mm (23) met youngs modulus
+
+#nummer van eigenfreq van betreffende draad
+x = np.array([1,2,3,4,5,6,7,8]) 
+#gevonden eigenfrequenties
+y = efk3
+
+#alle onzekerheden
+sig_x = np.array([0.0001,0.0001,0.0001,0.0001,0.0001,0.0001,0.0001,0.0001])
+sig_y = np.array([1,1,1,1,1,1,1,1])/3
+	
+# Dit is een niet opgemaakte sub plot voor de stippen
+f,ax = plt.subplots(1)
+ax.errorbar(x,y,xerr=sig_x,yerr=sig_y,fmt='k.')
+#ax.show()
+freq_start=0
+	
+# de functie/relatie
+def f(x,freq):
+    ef = x*freq
+    return ef
+	
+#model-object dat we gebruiken in odr
+odr_model = odr.Model(f)
+	
+#real data opbject
+odr_data  = odr.RealData(x,y,sx=sig_x,sy=sig_y)
+	
+# start waarde voor parameters
+odr_obj   = odr.ODR(odr_data,odr_model,beta0=[freq_start])
+
+
+odr_res   = odr_obj.run()
+	
+# de beste schatters voor de parameters
+par_best = odr_res.beta
+
+# de onzekerheden voor deze parameters
+par_sig_ext = odr_res.sd_beta
+
+# covariantiematrix
+par_cov_ext = odr_res.cov_beta 
+
+# De chi-kwadraat en de gereduceerde chi-kwadraat van deze fit
+chi2 = odr_res.sum_square
+chi2red = odr_res.res_var
+
+# Een compacte weergave van de resultaten als output
+# odr_res.pprint()
+
+# Hier plotten we ter controle de fit met de dataset met opmaak
+# En printen we de gereduceerde chi kwadraat uit.
+print('De gereduceerde chi-kwadraat van k3 is:', chi2red)
+xplot=np.linspace(1,8, 8)
+ax.plot(xplot, f([par_best[0]],xplot),'r-')
